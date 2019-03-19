78984d8d053fb6e6d0129532766b30dd15e358b0
Adam Hadar <hadar.adam@gmail.com>
2019-03-18 23:55:25 -0700

    Local DB queries

--- criterion-closet-picks.py
+++ criterion-closet-picks.py
@@ -1,8 +1,11 @@
-# fb id
-# 508242826257686
+#!/usr/local/bin/python3
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import source.compile as compile
+import source.searcher as Searcher
+import source.getter as Getter
+
 
 class server(BaseHTTPRequestHandler):
         def do_GET(self):
@@ -12,10 +15,28 @@ class server(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(bytes(document['body'], 'utf-8'))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
         from sys import argv
-        if len(argv) == 2 and argv[1] == 'build':
+
+        if argv[1] == 'build':
                 with open('index.html', 'w') as f:
                         f.write(compile.generate('/index.html')['body'])
-        elif len(argv) == 2 and argv[1] == 'host':
-                HTTPServer(('127.0.0.1', 3000), server).serve_forever()
+
+        elif argv[1] == 'host':
+                HTTPServer(('127.0.0.1', 1540), server).serve_forever()
+
+        elif argv[1] == 'search':
+                searcher = Searcher.searcher(argv[2])
+                searcher.run(argv[3])
+                searcher.write()
+
+        elif argv[1] == 'get':
+                getter = Getter.getter(argv[2])
+                getter.run(argv[3])
+                getter.write()
+
+        elif argv[1] == 'add':
+                getter = Getter.getter(argv[2])
+                getter.run(argv[3])
+                getter.write()
+
