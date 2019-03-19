cf09e2c052454e3915dce827b5d309e33bdd5dae
Adam Hadar <hadar.adam@gmail.com>
2019-03-18 23:54:54 -0700

    compile is more readable

--- source/compile.py
+++ source/compile.py
@@ -1,29 +1,63 @@
+class DOMString:
+        def __init__(self, strng):
+                self.str = strng
+
+        def write(self):
+                return self.str
+
+class DOMElement:
+        def __init__(self, name, attrs={}):
+                self.name = name
+                self.attrs = attrs
+                self.children = []
+
+        def appendChild(self, child):
+                self.children.append(child)
+                return self
+
+        def firstChild(self):
+                return self.children[0]
+
+        def lastChild(self):
+                return self.children[-1]
+
+        def write(self):
+                element = f'<{self.name}'
+                for key in self.attrs:
+                        element += ' '+key+'="'+self.attrs[key]+'"'
+                if self.name == 'meta':
+                        element += '/>'
+                else:
+                        element += '>'
+                        for child in self.children:
+                                element += child.write()
+                        element += f'</{self.name}>'
+                return element
+
 def generate(path):
         requested_path = path.split('/')
         if len(requested_path) > 1 and requested_path[1] not in ['index.html', '']:
                 return {'response_type': 404, 'body': 'File Not Found'}
         # Header data
-        index_file = '<!DOCTYPE html>' + \
-                     '<html class="night-mode">'+\
-                     '<meta charset="utf-8"/>'+\
-                     '<meta name="theme-color" content="#E9967A"/>'+\
-                     '<meta name="twitter:card" content="summary"/>'+\
-                     '<meta name="twitter:site" content="@radamadah"/>'+\
-                     '<meta name="twitter:creator" content="@radamadah"/>'+\
-                     '<meta name="twitter:title" content="Criterion Closet Picks Data Visualization"/>'+\
-                     '<meta name="twitter:description" content="A quick graphic that shows the movie tastes of visitors to the Criterion Collection\'s DVD Closet."/>'+\
-                     '<meta name="twitter:image" content="chart.png"/>'+\
-                     '<meta property="og:title" content="Criterion Closet Picks Data Visualization" />'+\
-                     '<meta property="og:description" content="A quick graphic that shows the movie tastes of visitors to the Criterion Collection\'s DVD Closet." />'+\
-                     '<meta property="og:type" content="website" />'+\
-                     '<meta property="og:url" content="http://adamhadar.me/things/criterion-closet-picks/" />'+\
-                     '<meta property="og:image" content="http://adamhadar.me/things/criterion-closet-picks/chart.png" />'+\
-                     '<title>Criterion Closet Picks Data Viz</title>'
+        index_file = DOMElement('html', { 'class': 'night-mode' })
+        index_file.appendChild(DOMElement('meta', { 'charset': 'utf-8' }))
+        index_file.appendChild(DOMElement('meta', { 'name': 'theme-color', 'content': '#E9967A' }))
+        index_file.appendChild(DOMElement('meta', { 'name': 'twitter:card', 'content': 'summary' }))
+        index_file.appendChild(DOMElement('meta', { 'name': 'twitter:site', 'content': '@radamadah' }))
+        index_file.appendChild(DOMElement('meta', { 'name': 'twitter:title', 'content': 'Criterion Closet Picks Data Visualization' }))
+        index_file.appendChild(DOMElement('meta', { 'name': 'twitter:description', 'content': 'A quick graphic that shows the movie tastes of visitors to the Criterion Collection\'s DVD Closet.' }))
+        index_file.appendChild(DOMElement('meta', { 'name': 'twitter:image', 'content': 'chart.png' }))
+        index_file.appendChild(DOMElement('meta', { 'property': 'og:title', 'content': 'Criterion Closet Picks Data Visualization' }))
+        index_file.appendChild(DOMElement('meta', { 'property': 'og:description', 'content': 'A quick graphic that shows the movie tastes of visitors to the Criterion Collection\'s DVD Closet.' }))
+        index_file.appendChild(DOMElement('meta', { 'property': 'og:type', 'content': 'website' }))
+        index_file.appendChild(DOMElement('meta', { 'property': 'og:url', 'content': 'http://adamhadar.me/things/criterion-closet-picks/' }))
+        index_file.appendChild(DOMElement('meta', { 'property': 'og:image', 'content': 'http://adamhadar.me/things/criterion-closet-picks/chart.png' }))
+        index_file.appendChild(DOMElement('title').appendChild(DOMString('Criterion Closet Picks Data Viz')))
         # Stylesheet
         with open('source/style.css') as f:
-                index_file += '<style>'+f.read()+'</style>'
+                index_file.appendChild(DOMElement('style').appendChild(f.read()))
         # Scripts
-        index_file += '<script src="https://d3js.org/d3.v4.min.js"></script>'
+        index_file.appendChild( DOMElement('script', { 'src': 'https://d3js.org/d3.v4.min.js' }) )
         visitsObj = ''
         moviesObj = ''
         countriesObj = ''
@@ -64,10 +98,8 @@ def generate(path):
                 for line in csv:
                         directors.append('{name: "'+line.rstrip()+'"}')
                 directorsObj = '['+','.join(directors)+']'
+        index_file.appendChild( DOMElement('script').appendChild('document.addEventListener("DOMContentLoaded", function(){ run({visits: '+visitsObj+', countries: '+countriesObj+', directors: '+directorsObj+', movies: '+moviesObj+'}); });') )
         with open('source/script.js') as f:
-                index_file += '<script>'
-                index_file += 'document.addEventListener("DOMContentLoaded", function(){ run({visits: '+visitsObj+', countries: '+countriesObj+', directors: '+directorsObj+', movies: '+moviesObj+'}); });'
-                index_file += f.read()
-                index_file += '</script>'
-        index_file += '</html>'
-        return {'response_type': 200, 'body': index_file}
+                index_file.appendChild( DOMElement('script').appendChild(f.read()) )
+        # index_file += '</html>'
+        return {'response_type': 200, 'body': '<!DOCTYPE html>'+index_file.write() }
