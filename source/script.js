c034229d42e3eb62feacf24cb4f80f39df8964a0
Adam Hadar <adam.hadar@columbia.edu>
2019-11-22 00:20:58 -0800

    using DOM, removing d3 usage, adding compliance to new website format

--- source/script.js
+++ source/script.js
@@ -1,66 +1,73 @@
+function DOM(){var i=function(_,v){return v===typeof _},s=null,o=function(_){return!i(_,'undefined')&&s!==_},g='insertBefore',r=function(_,v){return o(_)&&i(o(v)?v:_[g],'function')},a=arguments,m=a[0],t=a.length,e=1<t&&r(a[1])?a[1]:s,n=2<t&&r(a[2])?a[2]:s,y=a[t-1];return 0<t&&5>t&&o(m)&&i(m,'string')?m=document.createElement(m):s,r(m)?((1<t&&r(y,y)?y(m):s),(o(e)?e[g](m,n):s),m):s;}
+function DOMText(a){return document.createTextNode(a);}
+
 function run(jason){
+	var main = document.getElementsByTagName('main')[0];
 	var jason = preprocessData(jason);
-	[
-		Title(),
-		ChartView(jason),
-		OverView(jason),
-		DirectorListView(jason), CountryListView(jason),
-		VisitListView(jason), MovieListView(jason),
-		FooterView()
-	].forEach(function(o){ document.body.appendChild(o) });
+
+	LegendView(main);
+	ChartView(jason, main);
+	OverView(jason, main);
+	DirectorListView(jason, main);
+	CountryListView(jason, main);
+	VisitListView(jason, main);
+	MovieListView(jason, main);
 }
-function ListView(items, title, isValidFunc, listItemNumFunc, listItemNameFunc, listItemPopupFunc, sortFuncMap){
-	var element = document.createElement('data');
-	element.appendChild(document.createElement('h2')).innerHTML = title + ' <small>sort by </small>';
-	let DOMList = element.appendChild(document.createElement('ol'));
-	var DOMDetails = DOMList.getElementsByTagName('detail');
-	var DOMButtons = DOMList.getElementsByTagName('button');
-	let sortButtonsContainer = element.getElementsByTagName('small')[0];
-	let sortButtons = sortButtonsContainer.getElementsByTagName('button');
-	var sorts = [];
-	Object.keys(sortFuncMap).forEach(function(key){
-		let sortButton = document.createElement('button');
-		sortButton.innerHTML = key;
-		sortButtonsContainer.appendChild(sortButton);
-		let sort = function(){
-			DOMList.innerHTML = '';
-			items.sort( sortFuncMap[key] );
-			items.forEach(function(it){
-				if(isValidFunc(it) == 0)
-					return;
-				let li = document.createElement('li');
-				let innerHTML = '<button>'+listItemNumFunc(it)+' <arrow></arrow></button> ';
-				innerHTML += '<info><inline-bar style="width: '+isValidFunc(it)*3+'px">'+listItemNameFunc(it)+'</inline-bar></info><detail>'+listItemPopupFunc(it)+'</detail>';
-				li.innerHTML = innerHTML;
-				// Only one item expanded at a time.
-				let button = li.getElementsByTagName('button')[0];
-				let detail = li.getElementsByTagName('detail')[0];
-				detail.style.display = 'none';
-				button.addEventListener('click', function(){
-					if(detail.style.display == 'none'){
-						for(var i = 0; i < DOMDetails.length; ++i){
-							DOMDetails[i].style.display = 'none';
-							DOMButtons[i].classList = '';
-						}
-						detail.style.display = 'block';
-						button.classList = 'active';
-					}
-					else {
-						detail.style.display = 'none';
-						button.classList = '';
-					}
-				});
-				DOMList.appendChild(li);
-			});
-			for(let i = 0; i < sortButtons.length; ++i)
-				sortButtons[i].classList = '';
-			sortButton.classList = 'active';
-		};
-		sorts.push(sort);
-		sortButton.addEventListener('click', sort);
-	});
-	sorts[0]();
-	return element;
+function ListView(parent, items, title, isValidFunc, listItemNumFunc, listItemNameFunc, listItemPopupFunc, sortFuncMap){
+	DOM('data', parent, (data) => {
+		let sortButtonsContainer = null;
+		DOM('h2', data, (h2) => {
+			DOM(DOMText(title + ' '), h2)
+			sortButtonsContainer = DOM('small', h2, small => small.innerHTML = 'sort by ')
+		})
+		let DOMList = DOM('ol', data)
+		var DOMDetails = DOMList.getElementsByTagName('detail');
+		var DOMButtons = DOMList.getElementsByTagName('button');
+		let DOMSortButtons = sortButtonsContainer.getElementsByTagName('button');
+		var sorts = [];
+		Object.keys(sortFuncMap).forEach(function(key){
+			DOM('button', sortButtonsContainer, sortButton => {
+				sortButton.innerHTML = key;
+				let sort = function(){
+					DOMList.innerHTML = '';
+					items.sort(sortFuncMap[key]);
+					items.forEach(function(it){
+						if(isValidFunc(it) == 0)
+							return;
+						DOM('li', DOMList, li => {
+							let innerHTML = '<button>'+listItemNumFunc(it)+' <arrow></arrow></button> ';
+							innerHTML += '<info><inline-bar style="width: '+isValidFunc(it)*3+'px">'+listItemNameFunc(it)+'</inline-bar></info><detail>'+listItemPopupFunc(it)+'</detail>';
+							li.innerHTML = innerHTML;
+							// Only one item expanded at a time.
+							let button = li.getElementsByTagName('button')[0];
+							let detail = li.getElementsByTagName('detail')[0];
+							detail.style.display = 'none';
+							button.addEventListener('click', function(){
+								if(detail.style.display == 'none'){
+									for(var i = 0; i < DOMDetails.length; ++i){
+										DOMDetails[i].style.display = 'none';
+										DOMButtons[i].classList = '';
+									}
+									detail.style.display = 'block';
+									button.classList = 'active';
+								}
+								else {
+									detail.style.display = 'none';
+									button.classList = '';
+								}
+							});
+						})
+					});
+					for(let i = 0; i < DOMSortButtons.length; ++i)
+						DOMSortButtons[i].classList = '';
+					sortButton.classList = 'active';
+				};
+				sorts.push(sort);
+				sortButton.addEventListener('click', sort);
+			})
+		});
+		sorts[0]();
+	})
 }
 function popularSet(list, comparator){
 	let popular = [];
@@ -86,7 +93,7 @@ function prettyList(list){
 function comparAttr(a,b,c){ return a[c] > b[c]? 1 : b[c] > a[c]? -1 : 0; }
 function comparArray(a,b,c){ return a[c].length > b[c].length? 1 : b[c].length > a[c].length? -1 : 0; }
 function preprocessData(jason){
-	window.margin = {top: 30, right: 14, bottom: 20, left: 30};
+	window.margin = {top: 30, right: 30, bottom: 30, left: 40};
 	window.SPINE_COUNT = 1100;
 	window.VISIT_COUNT = Math.ceil(Math.sqrt(Object.keys(jason.visits).length)) + 2;
 	window.width = SPINE_COUNT*1.3 - margin.left - margin.right;
@@ -151,70 +158,116 @@ function preprocessData(jason){
 	});
 	return jason;
 }
-function Title(){
-	var element = document.createElement('h1');
-	element.innerHTML = 'Criterion Closet Picks Data Visualization';
-	return element;
+function LegendView(parent){
+	DOM('legend', parent, (legend) => {
+		DOM('strong', legend, strong => strong.innerHTML = 'Legend')
+		DOM('div', legend, (div) => {
+			DOM('box', div, box => box.classList = 'data1-background')
+			DOM(DOMText('movies bagged'), div)
+		});
+		DOM('div', legend, (div) => {
+			DOM('box', div, box => box.classList = 'data2-background')
+			DOM(DOMText('movies mentioned'), div)
+		})
+	})
 }
-function ChartView(jason){
-	var element = document.createElement('chart');
-	element.innerHTML = '<svg/><div><svg id="chart"/></div><legend><strong>Legend</strong><div><box class="bagged"></box>movies bagged</div><div><box class="mentioned"></box>movies mentioned</div></legend>';
-	var y_axis = d3.select(element.getElementsByTagName('svg')[0])
-		.attr("width", margin.left)
-		.attr("height", height+margin.top+margin.bottom)
-		.append("g").attr("transform", "translate("+29+","+margin.top+")");
-	var y_axis_domain = Array.apply(null, Array(VISIT_COUNT)).map(function(_,i){ return i });
-	var y_axis_range = Array.apply(null, Array(VISIT_COUNT)).map(function(_,i){ return height*(1 - i/VISIT_COUNT) });
-	var y_scale = d3.scaleOrdinal().domain(y_axis_domain).range(y_axis_range);
-	y_axis.append("g").attr("class", "axis").call(d3.axisLeft(y_scale));
-	var chart = d3.select(element.getElementsByTagName('svg')[1])
-		.attr("width", width + margin.right).attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate("+margin.left/12+","+margin.top+")");
-	var axis_max = SPINE_COUNT/100;
-	var domain_axis = Array.apply(null, Array(axis_max)).map(function(_,i){ return i*100 });
-	var range_axis = Array.apply(null, Array(axis_max)).map(function(_,i){ return width*i/(axis_max-1) });
-	var x_scale = d3.scaleOrdinal().domain(domain_axis).range(range_axis);
-	var all = chart.selectAll(".bar").data(jason.movie_list).enter()
-		.append("g")
-		.attr("class", "bar")
-		.attr("transform", function(d){ return "translate("+d.x0+","+(height-d.length*bin_height)+")"});
-	all.append("rect").attr("class", "bagged")
-		.attr("transform", function(d){ return "translate(0," + d.mentioned*bin_height + ")" })
-		.attr("width", bin_width).attr("height", function(d){ return d.bagged*bin_height });
-	all.append("rect").attr("class", "mentioned")
-		.attr("width", bin_width).attr("height", function(d){ return d.mentioned*bin_height });
-	chart.append("g").attr("class", "axis")
-		.attr("transform", "translate(0,"+height+")").call(d3.axisBottom(x_scale));
-	return element;
+function ChartView(jason, parent){
+	DOM('chart', parent, chart => {
+		let yAxisTicks = '';
+		for(let i = 0; i < VISIT_COUNT; ++i) {
+			yAxisTicks += ''+
+				'<g class="tick" opacity="1" transform="translate(0,'+(height*(1 - i/VISIT_COUNT))+')">'+
+				'<line x2="-6"></line>'+
+				'<text x="-9" y="5">'+i+'</text>'+
+				'</g>';
+		}
+
+		let svg1 = ''+
+		'<svg width="'+margin.left+'" height="'+(height+margin.top+margin.bottom)+'">'+
+			'<g transform="translate(29,'+margin.top+')">'+
+				'<g class="axis" fill="none" text-anchor="end">'+
+					'<path class="domain" d="M0,'+height+'H0.5V'+(height/VISIT_COUNT)+'H0"></path>'+
+					yAxisTicks+
+				'</g>'+
+			'</g>'+
+		'</svg>';
+
+		chart.innerHTML = svg1;
+
+		let xAxisTicks = '';
+		let xAxisMax = SPINE_COUNT/100;
+		for(let i = 0; i < xAxisMax; i++) {
+			xAxisTicks += ''+
+			'<g class="tick" opacity="1" transform="translate('+(width*i/(xAxisMax-1))+',0)">'+
+				'<line y2="6"></line>'+
+				'<text y="9" dy="0.71em">'+(i*100)+'</text>'+
+			'</g>';
+		}
+
+		let bars = '';
+
+		jason.movie_list.forEach((movie) => {
+			bars += ''+
+			'<g id="bar-'+movie.spine_number+'" class="bar" transform="translate('+movie.x0+','+(height-movie.length*bin_height)+')">'+
+			'<rect class="data2" transform="translate(0,0)" width="'+bin_width+'" height="'+(movie.mentioned*bin_height)+'"></rect>'+
+			'<rect class="data1" transform="translate(0,'+(movie.mentioned*bin_height)+')" width="'+bin_width+'" height="'+(movie.bagged*bin_height)+'"></rect>'+
+			
+			'</g>';
+		})
+
+
+		DOM('div', chart, div => {
+			div.innerHTML = ''+
+			'<svg id="chart" width="'+(width + margin.right)+'" height="'+(height + margin.top + margin.bottom)+'">'+
+				'<g transform="translate('+(margin.left/12)+','+margin.top+')">'+
+					bars+
+					'<g class="axis" transform="translate(0,'+height+')" fill="none" text-anchor="middle">'+
+						'<path class="domain" d="M0.5,6V0.5H'+width+'V0"></path>'+
+						xAxisTicks+
+					'</g>'+
+				'</g>'+
+			'</svg>';
+		})
+	})
 }
-function OverView(jason){
-	var element = document.createElement('analysis');
+function OverView(jason, parent){
 	let movies = popularSet(jason.movie_list, function(m){ return m.length });
 	let directors = popularSet(jason.directors, function(d){ return d.movies.length });
 	let countries = popularSet(jason.countries, function(c){ return c.movies.length });
 	let i18n = {
 		movies_count: (movies.popular.length > 1) ? 'movies' : 'movie',
 		movies_be: (movies.popular.length > 1) ? 'are' : 'is',
-		movies_list: prettyList(movies.popular.map(function(m){ return '<cite class="interactive">'+m.title+'</cite>' })),
+		movies_list: prettyList(movies.popular.map(function(m){ return '<cite class="active">'+m.title+'</cite>' })),
 		apiece: (movies.popular.length > 1) ? ' apiece' : '',
 		director: (directors.popular.length > 1) ? 'directors' : 'director',
 		directors_be: (directors.popular.length > 1) ? 'are' : 'is',
-		directors_list: prettyList(directors.popular.map(function(d){ return '<span class="interactive">'+d.name+'</span>' })),
+		directors_list: prettyList(directors.popular.map(function(d){ return '<span class="active">'+d.name+'</span>' })),
 		country: (countries.popular.length > 1) ? 'countries' : 'country',
 		countries_be: (countries.popular.length > 1) ? 'are' : 'is',
-		countries_list: prettyList(countries.popular.map(function(c){ return '<span class="interactive">'+c.name+'</span>' })),
+		countries_list: prettyList(countries.popular.map(function(c){ return '<span class="active">'+c.name+'</span>' })),
 		each: (countries.popular.length > 1) ? ' each' : ''
 	};
-	element.appendChild(document.createElement('p')).innerHTML = 'The aim of this page is to easily visualize the movie tastes of the various visitors of the <a href="https://www.criterion.com/">Criterion Collection</a>\'s closet, as seen in <a href="https://www.youtube.com/playlist?list=PLFk8lnn1nEEID1NNh7vCZuU5Y4r2YrheR">this video series</a>. The chart above shows each movie picked, in Criterion spine # order, against the number of times they were picked, while the interactive lists below expose the dataset.';
-	element.appendChild(document.createElement('p')).innerHTML = 'There have been <span class="interactive">'+jason.stats.visitsCount+'</span> visits to the Criterion closet, from which <span class="interactive">'+jason.stats.moviesCount+'</span> unique films by <span class="interactive">'+jason.stats.directorsCount+'</span> unique directors in <span class="interactive">'+jason.stats.countriesCount+'</span> unique countries have been picked up/discussed.';
-	element.appendChild(document.createElement('p')).innerHTML = i18n.movies_list+' '+i18n.movies_be+' the most popular '+i18n.movies_count+' among closet visitors, with <span class="interactive">'+movies.value+'</span> baggings/mentions'+i18n.apiece+'.';
-	element.appendChild(document.createElement('p')).innerHTML = i18n.directors_list+' '+i18n.directors_be+' the most popular '+i18n.director+', with <span class="interactive">'+directors.value+'</span> baggings/mentions.';
-	element.appendChild(document.createElement('p')).innerHTML = 'The '+i18n.country+' from which the most movies are bagged/mentioned '+i18n.countries_be+' '+i18n.countries_list+' (<span class="interactive">'+countries.value+'</span> movies'+i18n.each+').';
-	return element;
+	DOM('analysis', parent, (analysis) => {
+		DOM('p', analysis, (p) => {
+			p.innerHTML = 'The aim of this page is to easily visualize the movie tastes of the various visitors of the <a href="https://www.criterion.com/">Criterion Collection</a>\'s closet, as seen in <a href="https://www.youtube.com/playlist?list=PLFk8lnn1nEEID1NNh7vCZuU5Y4r2YrheR">this video series</a>. The chart above shows each movie picked, in Criterion spine # order, against the number of times they were picked, while the interactive lists below expose the dataset.';
+		})
+		DOM('p', analysis, (p) => {
+			p.innerHTML = 'There have been <span class="active">'+jason.stats.visitsCount+'</span> visits to the Criterion closet, from which <span class="active">'+jason.stats.moviesCount+'</span> unique films by <span class="active">'+jason.stats.directorsCount+'</span> unique directors in <span class="active">'+jason.stats.countriesCount+'</span> unique countries have been picked up/discussed.';
+		})
+		DOM('p', analysis, (p) => {
+			p.innerHTML = i18n.movies_list+' '+i18n.movies_be+' the most popular '+i18n.movies_count+' among closet visitors, with <span class="active">'+movies.value+'</span> baggings/mentions'+i18n.apiece+'.';
+		})
+		DOM('p', analysis, (p) => {
+			p.innerHTML = i18n.directors_list+' '+i18n.directors_be+' the most popular '+i18n.director+', with <span class="active">'+directors.value+'</span> baggings/mentions.';
+		})
+		DOM('p', analysis, (p) => {
+			p.innerHTML = 'The '+i18n.country+' from which the most movies are bagged/mentioned '+i18n.countries_be+' '+i18n.countries_list+' (<span class="active">'+countries.value+'</span> movies'+i18n.each+').';
+		})
+	});
 }
-function DirectorListView(jason){
+function DirectorListView(jason, parent){
 	return ListView(
+		parent,
 		jason.directors,
 		'Director List',
 		function(it){ return it.movies.length },
@@ -227,8 +280,9 @@ function DirectorListView(jason){
 		}
 	);
 }
-function CountryListView(jason){
+function CountryListView(jason, parent){
 	return ListView(
+		parent,
 		jason.countries,
 		'Country List',
 		function(it){ return it.movies.length },
@@ -241,38 +295,35 @@ function CountryListView(jason){
 		}
 	)
 }
-function VisitListView(jason){
+function VisitListView(jason, parent){
 	return ListView(
+		parent,
 		jason.visit_list,
 		'Visit List',
 		function(it){ return it.all_movies.length },
 		function(it){ return it.date },
 		function(it){ return it.visitor },
-		function(it){ let movieList=prettyList(it.all_movies.map(function(m){ return '<cite class="'+(it.spines_bagged.indexOf(m.spine_number)==-1?'mentioned':'bagged')+'">'+m.title+'</cite>'; }))+'.'; return '<a href="https://youtu.be/'+it.url+'">Video link</a>.<br/>'+movieList + (it.notes.length > 0? '<hr/>'+it.notes : ''); },
+		function(it){ let movieList=prettyList(it.all_movies.map(function(m){ return '<cite class="'+(it.spines_bagged.indexOf(m.spine_number)==-1?'data2':'data1')+'">'+m.title+'</cite>'; }))+'.'; return '<a href="https://youtu.be/'+it.url+'">Video link</a>.<br/>'+movieList + (it.notes.length > 0? '<hr/>'+it.notes : ''); },
 		{
 			'date': function(a,b){ return comparAttr(a,b, 'date') },
 			'alphabetically': function(a,b){ return comparAttr(a,b, 'visitor') },
 			'greediness': function(a,b){ let v = comparArray(b,a,'all_movies'); if(v == 0) v = comparAttr(a,b,'name'); return v; }
 		}
 	)
-};
-function MovieListView(jason){
+}
+function MovieListView(jason, parent){
 	return ListView(
+		parent,
 		jason.movie_list,
 		'Movie List',
 		function(it){ return it.visitors.length },
 		function(it){ return '<list-num>'+it.spine_number+'</list-num>' },
 		function(it){ return '<cite>'+it.title+'</cite>' },
-		function(it){ return 'Directed by '+prettyList(it.directors.map(function(d){return d.name}))+'. '+it.year+'. <a href="https://criterion.com/films/'+it.url+'">Criterion link</a>.<br/>'+ prettyList(it.visitors.map(function(v){ return '<cite class="'+(v.spines_bagged.indexOf(it.spine_number)==-1?'mentioned':'bagged')+'">'+v.visitor+'</cite>' }))+'.' },
+		function(it){ return 'Directed by '+prettyList(it.directors.map(function(d){return d.name}))+'. '+it.year+'. <a href="https://criterion.com/films/'+it.url+'">Criterion link</a>.<br/>'+ prettyList(it.visitors.map(function(v){ return '<cite class="'+(v.spines_bagged.indexOf(it.spine_number)==-1?'data2':'data1')+'">'+v.visitor+'</cite>' }))+'.' },
 		{
 			'spine #': function(a,b){ return comparAttr(a,b,'spine_number') },
 			'popularity': function(a,b){ let v = comparArray(b,a,'visitors'); if(v==0)v=comparAttr(a,b,'spine_number'); return v; },
 			'alphabetically': function(a,b){ return comparAttr(a,b,'title') }
 		}
 	)
-};
-function FooterView(){
-	var element = document.createElement('footer');
-	element.appendChild(document.createElement('small')).innerHTML = 'The data for this chart was gathered by hand -- if there are any omissions or errors, let me know: <a href="https://twitter.com/radamadah">@radamadah</a>.</br>Coded with d3.js. If you want to contribute to this thing, it\'s a public repo on Github (<a href="https://github.com/radamadah/criterion-closet-picks">https://github.com/radamadah/criterion-closet-picks</a>), and I\'ll take any good PRs! Last updated 2019-11-05.';
-	return element;
 }
